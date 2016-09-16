b3e81861bc463c8a163c619f0ef1cf5aa1d7eb85
Markus Mattinen <markus.mattinen@vincit.fi>
2016-09-16 16:39:35 +0300

    Use directories instead of branches for different versions

---
+++ 8.1/config/var/www/owncloud/autoconfig.php
@@ -0,0 +1,7 @@
+<?php
+AUTOCONFIG = array(
+    "directory"       => "/var/www/owncloud/data",
+    "logfile"         => "/dev/stdout",
+    "loglevel"        => "INFO",
+    "log_rotate_size" => false
+);
